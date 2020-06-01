ba4538723b3f28d8ab16984a0ca1c1b40113b109
leosanqing <stormleo@qq.com>
2020-06-01 19:32:54 +0800

    1. 添加 205题 和206题

--- java-note-algorithm/src/main/java/com/leosanqing/leetcode/easy/_205_isomorphicStrings.java
+++ java-note-algorithm/src/main/java/com/leosanqing/leetcode/easy/_205_isomorphicStrings.java
@@ -82,4 +82,40 @@ public class _205_isomorphicStrings {
     }
 
 
+    /**
+     * 大佬们的算法，据说时间 3ms，
+     * 这边是使用 数组，
+     * @param sString
+     * @param tString
+     * @return
+     */
+    public boolean isIsomorphic2(String sString, String tString) {
+
+        char[] s = sString.toCharArray();
+        char[] t = tString.toCharArray();
+
+        int length = s.length;
+        if(length != t.length) {
+            return false;
+        }
+
+        char[] sm = new char[256];
+        char[] tm = new char[256];
+
+        for(int i=0; i<length; i++){
+            char sc = s[i];
+            char tc = t[i];
+            // 如果
+            if(sm[sc] == 0 && tm[tc] == 0){
+                sm[sc] = tc;
+                tm[tc] = sc;
+            }else{
+                if(sm[sc] != tc || tm[tc] != sc){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
---
+++ java-note-algorithm/src/main/java/com/leosanqing/leetcode/easy/_206_reverseLinkedList.java
@@ -0,0 +1,67 @@
+package com.leosanqing.leetcode.easy;
+
+/**
+ * @Author: rtliu
+ * @Date: 2020/6/1 下午5:51
+ * @Package: com.leosanqing.leetcode.easy
+ * @Description: Reverse a singly linked list.
+ * 反转单链表
+ * @Version: 1.0
+ * <p>
+ * Example:
+ * <p>
+ * Input: 1->2->3->4->5->NULL
+ * Output: 5->4->3->2->1->NULL
+ */
+public class _206_reverseLinkedList {
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2, node1);
+        ListNode node3 = new ListNode(3, node2);
+        ListNode node4 = new ListNode(4, node3);
+        ListNode node5 = new ListNode(5, node4);
+
+        ListNode listNode = reverseList(node5);
+
+        while(listNode!=null){
+            System.out.println(listNode.val);
+            listNode= listNode.next;
+        }
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        while(head != null){
+
+            // 顺序不能反
+            ListNode next = head.next;
+            head.next = pre;
+
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
+
+
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
